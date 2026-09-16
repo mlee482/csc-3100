829fcf080b7adc15eda3419ebe91e6e837813b48
Madison Lee <madisonlee@Mac-615.lan>
2026-09-16 14:50:44 -0700

    Added POST calls to add users update list

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
@@ -6,22 +6,47 @@ function MyApp() {
   const [characters, setCharacters] = useState([]);
 
   function removeOneCharacter(index) {
-    const updated = characters.filter((character, i) => {
-      return i !== index;
-    });
+    const updated = characters.filter((character, i) => i !== index);
     setCharacters(updated);
   }
 
   function updateList(person) {
-  setCharacters([...characters, person]);
+  postUser(person)
+    .then(() => setCharacters([...characters, person]))
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
+  function fetchUsers() {
+    return fetch("http://localhost:8000/users");
+  }
+
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(person),
+  });
+
+  return promise;
+}
+
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
-  <div className="container">
-    <Table characterData={characters} removeCharacter={removeOneCharacter} />
-    <Form handleSubmit={updateList} />
-  </div>
-);
+    <div className="container">
+      <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form handleSubmit={updateList} />
+    </div>
+  );
 }
 
 export default MyApp;
