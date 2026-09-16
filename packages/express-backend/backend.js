56d0aba09307d32cc9bf22e781f4d5c9a923b4d1
Madison Lee <madisonlee@Mac-615.lan>
2026-09-16 14:51:11 -0700

    CORS added

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,7 +1,10 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
 const port = 8000;
+
 const users = {
   users_list: [
     {
@@ -60,6 +63,8 @@ const matchUser = (name, job) => {
     (user => user["name"] === name && user["job"] === job)
   );
 }
+
+app.use(cors());
 
 app.use(express.json());
 
